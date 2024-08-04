6d073af95f774b245c223a048bf737e1bfdec714
Thiago Fernando <thiago.fernando@msn.com>
2024-08-04 15:31:56 -0400

    Criado getEntityManager

--- src/main/kotlin/br/com/alura/alugames/dados/Banco.kt
+++ src/main/kotlin/br/com/alura/alugames/dados/Banco.kt
@@ -1,15 +1,11 @@
 package br.com.alura.alugames.dados
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.SQLException
+import javax.persistence.EntityManager
+import javax.persistence.EntityManagerFactory
+import javax.persistence.Persistence
 
 object Banco {
-    fun obterConexao(): Connection?{
-        return  try {
-            DriverManager.getConnection("jdbc:mysql://localhost:3306/Alugames","thiago","Mudar@1234")
-        }catch (e: SQLException){
-            e.printStackTrace()
-            null
-        }
+    fun getEntityManager(): EntityManager {
+        val factory: EntityManagerFactory = Persistence.createEntityManagerFactory("alugames")
+        return factory.createEntityManager()
     }
 }
