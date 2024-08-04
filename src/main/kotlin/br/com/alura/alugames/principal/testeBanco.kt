854640184e24e75e5305f02e180f8c8eff3a2ce0
Thiago Fernando <thiago.fernando@msn.com>
2024-08-04 17:15:53 -0400

    Ajuste no teste banco

--- src/main/kotlin/br/com/alura/alugames/principal/testeBanco.kt
+++ src/main/kotlin/br/com/alura/alugames/principal/testeBanco.kt
@@ -7,9 +7,9 @@ fun main(){
     val jogoNovo = Jogo("STAR WARS: Galaxy of Heroes","https://media.contentapi.ea.com/content/dam/eacom/star-wars/images/2019/10/sw-inline-media-franchise-hub-swgoh.png.adapt.crop16x9.1455w.png",332.99,"Viva seus sonhos de STAR WARS™ e lute em locais icônicos com seus heróis e heroínas favoritos, do lado sombrio e do lado da luz, para se tornar mestre da galáxia.")
     val manager = Banco.getEntityManager()
     val jogoDao = JogosDAO(manager)
-    jogoDao.adicionarJogo(jogoNovo)
+    jogoDao.adicionar(jogoNovo)
 
-    val listJogos:List<Jogo> = jogoDao.getJogos()
+    val listJogos:List<Jogo> = jogoDao.getLista()
     println(listJogos)
     println("No banco tem ${listJogos.size} jogo(s) cadastrado(s)")
     manager.close()
